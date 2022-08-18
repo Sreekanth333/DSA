47449d180d9fa549c13248f5f82df1c3e664373f
Sreekanth <sreekanthnaidu003@gmail.com>
2022-08-18 22:51:12 +0530

    Updated BubbleSort.java

--- src/com/sreekanth/SortingAlgos/BubbleSort.java
+++ src/com/sreekanth/SortingAlgos/BubbleSort.java
@@ -5,19 +5,19 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {9,4,7,3,1};
-        System.out.println(Arrays.toString(bubbleSort(arr)));
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
-    static int[] bubbleSort(int[] arr){
+    static void bubbleSort(int[] arr){
         int n = arr.length;
-        for (int i = 0;i<n-1 ;i++ ){
-            for (int j = 0;j<n-i-1 ;j++ ){
-                if(arr[j] > arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+        for (int i = 0;i<n ;i++ ){
+            for (int j = 1;j<n-i ;j++ ){
+                if(arr[j-1] > arr[j]){
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
-        return arr;
     }
 }
